5aabc2d560a287639f7983e91ce9a9d746aff0d0
Simon Hoffmann <shoffmann@uni-wuppertal.de>
2022-08-22 16:05:32 +0200

    tests OpenCLHelper

--- test/test_opencl_helper.cpp
+++ test/test_opencl_helper.cpp
@@ -24,4 +24,38 @@ TEST_CASE("OpenCLHelper")
         REQUIRE(openCLHelper.getKernelDir() == "./");
         REQUIRE(openCLHelper.getProgram() != nullptr);
     }
+
+    SECTION("init kernelDir")
+    {
+        std::string filePath = __FILE__;
+        std::string dirPath = filePath.substr(0, filePath.rfind("/"));
+        openCLHelper.setKernelDir(dirPath.append("/../build/"));
+
+        int err = openCLHelper.init();
+        REQUIRE(err == CL_SUCCESS);
+        REQUIRE(openCLHelper.getProgram() != nullptr);
+    }
+
+    SECTION("reusing OpenCL platform")
+    {
+        int err = openCLHelper.init();
+        REQUIRE(err == CL_SUCCESS);
+
+        mgcl::Cuboid v2(3, 3, 3);
+        mgcl::Cuboid f2(3, 3, 3);
+        mgcl::Problem p2(3, 3, 3, f2, v2);
+        p2.setReuseOpenclBuffers(true);
+        mgcl::OpenCLHelper openCLHelper2(&p2);
+
+        openCLHelper2.setContext(openCLHelper.getContext());
+        openCLHelper2.setCommands(openCLHelper.getCommands());
+        openCLHelper2.setDeviceId(openCLHelper.getDeviceId());
+
+        err = openCLHelper2.init();
+        REQUIRE(err == CL_SUCCESS);
+
+        REQUIRE(openCLHelper2.getContext() == openCLHelper.getContext());
+        REQUIRE(openCLHelper2.getCommands() == openCLHelper.getCommands());
+        REQUIRE(openCLHelper2.getDeviceId() == openCLHelper.getDeviceId());
+    }
 }
