0b959e56efcbde759e6bed034c5d3a8cfe4e73f9
Simon Hoffmann <shoffmann@fugg1.pleiades.uni-wuppertal.de>
2022-09-22 10:47:34 +0200

    run scripts for slurm

---
+++ run_benchmarks.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --exclusive
+#SBATCH --job-name=mgcl_bench
+#SBATCH --partition=gpu
+#SBATCH --account=imacm_gpu
+#SBATCH -N1
+#SBATCH -n1
+#SBATCH --gpus 1
+#SBATCH --gpus-per-task 1
+#SBATCH --cpus-per-task 1
+
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+
+# cd ~/output/ || exit
+
+# run e.g. with sbatch run_benchmarks.sh [solve][console]
+"$HOME"/projects/mgcl/build/benchmarks "$@" > ~/output/"$current_time"-mgcl-bench.txt
+
+# cd "$HOME"/projects/mgcl || exit
---
+++ run_clean_build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --exclusive
+#SBATCH --job-name=mgcl_build
+#SBATCH --partition=gpu
+#SBATCH --account=imacm_gpu
+#SBATCH -N1
+#SBATCH -n1
+#SBATCH --gpus 1
+#SBATCH --gpus-per-task 1
+#SBATCH --cpus-per-task 1
+
+# cd ~/output/ || exit
+
+cmake -DCMAKE_BUILD_TYPE=Release "$HOME"/projects/mgcl/build
+cmake --build --clean-first  "$HOME"/projects/mgcl/build
+
+# cd "$HOME"/projects/mgcl || exit
