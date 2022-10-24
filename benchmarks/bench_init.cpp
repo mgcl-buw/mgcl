eecf439097d86407fa0c43759c69e838cfb672e6
Simon Hoffmann <shoffmann@uni-wuppertal.de>
2022-10-24 11:28:39 +0200

    example main for galerkin for profiling + bench

--- benchmarks/bench_init.cpp
+++ benchmarks/bench_init.cpp
@@ -43,7 +43,7 @@ TEST_CASE("mgcl benchmarks console: init", "[!benchmark][init][console]")
             p.setSilent(true);
 
             bool ret;
-            b.run(std::string("sequential, N = ").append(std::to_string(N)).c_str(), [&]
+            b.run(std::string("sequential 7p Laplace, N = ").append(std::to_string(N)).c_str(), [&]
                   { ret = p.init(); });
             REQUIRE(ret);
         }
@@ -61,7 +61,7 @@ TEST_CASE("mgcl benchmarks console: init", "[!benchmark][init][console]")
             p.setSilent(true);
 
             bool ret;
-            b.run(std::string("opencl GPU, N = ").append(std::to_string(N)).c_str(), [&]
+            b.run(std::string("opencl GPU 7p Laplace, N = ").append(std::to_string(N)).c_str(), [&]
                   { ret = p.init(); });
             REQUIRE(ret);
         }
@@ -82,7 +82,7 @@ TEST_CASE("mgcl benchmarks console: init", "[!benchmark][init][console]")
             REQUIRE(p.initOpenCL() == CL_SUCCESS);
 
             bool ret;
-            b.run(std::string("opencl GPU reusing platform, N = ").append(std::to_string(N)).c_str(), [&]
+            b.run(std::string("opencl GPU reusing platform 7p Laplace, N = ").append(std::to_string(N)).c_str(), [&]
                   { ret = p.init(); });
             REQUIRE(ret);
         }
@@ -100,7 +100,7 @@ TEST_CASE("mgcl benchmarks console: init", "[!benchmark][init][console]")
             p.setSilent(true);
 
             bool ret;
-            b.run(std::string("opencl CPU, N = ").append(std::to_string(N)).c_str(), [&]
+            b.run(std::string("opencl CPU 7p Laplace, N = ").append(std::to_string(N)).c_str(), [&]
                   { ret = p.init(); });
             REQUIRE(ret);
         }
@@ -121,9 +121,125 @@ TEST_CASE("mgcl benchmarks console: init", "[!benchmark][init][console]")
             REQUIRE(p.initOpenCL() == CL_SUCCESS);
 
             bool ret;
-            b.run(std::string("opencl CPU reusing platform, N = ").append(std::to_string(N)).c_str(), [&]
+            b.run(std::string("opencl CPU reusing platform 7p Laplace, N = ").append(std::to_string(N)).c_str(), [&]
                   { ret = p.init(); });
             REQUIRE(ret);
         }
+
+        /* **********************************
+         * Varying stencil from here on
+         ************************************ */
+
+        {
+            auto v = std::make_shared<mgcl::Cuboid>(m, n, o);
+            auto f = std::make_shared<mgcl::Cuboid>(m, n, o);
+            v->fillRandom(0, 10);
+            f->fillRandom(0, 10);
+
+            mgcl::Problem p(m, n, o, f, v);
+            p.setSilent(true);
+            p.setStencilType(mgcl::MGCL_VARYING_7POINT);
+            auto &s = *p.getStencilValues();
+
+            // Fill with 7-point Laplace, which is also used by the other two Sections in this test case
+            for (int i = 0; i < N; i++)
+                for (int j = 0; j < N; j++)
+                    for (int k = 0; k < N; k++)
+                    {
+                        // 7-point Laplace
+                        s[i][j][k][0][1][1] = 1;
+                        s[i][j][k][1][0][1] = 1;
+                        s[i][j][k][1][1][0] = 1;
+                        s[i][j][k][1][1][1] = -6;
+                        s[i][j][k][1][1][2] = 1;
+                        s[i][j][k][1][2][1] = 1;
+                        s[i][j][k][2][1][1] = 1;
+                    }
+
+            bool ret;
+            b.run(std::string("sequential 7p Varying, N = ").append(std::to_string(N)).c_str(), [&]
+                  { ret = p.init(); });
+            REQUIRE(ret);
+        }
+
+        // TODO run also on gpu when implemented
+
+        // if (gpuAvailable)
+        // {
+        //     auto v = std::make_shared<mgcl::Cuboid>(m, n, o);
+        //     auto f = std::make_shared<mgcl::Cuboid>(m, n, o);
+        //     v->fillRandom(0, 10);
+        //     f->fillRandom(0, 10);
+
+        //     mgcl::Problem p(m, n, o, f, v);
+        //     p.setUseOpencl(true);
+        //     p.setDeviceType(CL_DEVICE_TYPE_GPU);
+        //     p.setSilent(true);
+
+        //     bool ret;
+        //     b.run(std::string("opencl GPU 7p Laplace, N = ").append(std::to_string(N)).c_str(), [&]
+        //           { ret = p.init(); });
+        //     REQUIRE(ret);
+        // }
+
+        // if (gpuAvailable)
+        // {
+        //     auto v = std::make_shared<mgcl::Cuboid>(m, n, o);
+        //     auto f = std::make_shared<mgcl::Cuboid>(m, n, o);
+        //     v->fillRandom(0, 10);
+        //     f->fillRandom(0, 10);
+
+        //     mgcl::Problem p(m, n, o, f, v);
+        //     p.setUseOpencl(true);
+        //     p.setDeviceType(CL_DEVICE_TYPE_GPU);
+        //     p.setSilent(true);
+
+        //     // this is equal to Problem::reuseOpenCL for our matter
+        //     REQUIRE(p.initOpenCL() == CL_SUCCESS);
+
+        //     bool ret;
+        //     b.run(std::string("opencl GPU reusing platform 7p Laplace, N = ").append(std::to_string(N)).c_str(), [&]
+        //           { ret = p.init(); });
+        //     REQUIRE(ret);
+        // }
+
+        // if (cpuAvailable)
+        // {
+        //     auto v = std::make_shared<mgcl::Cuboid>(m, n, o);
+        //     auto f = std::make_shared<mgcl::Cuboid>(m, n, o);
+        //     v->fillRandom(0, 10);
+        //     f->fillRandom(0, 10);
+
+        //     mgcl::Problem p(m, n, o, f, v);
+        //     p.setUseOpencl(true);
+        //     p.setDeviceType(CL_DEVICE_TYPE_CPU);
+        //     p.setSilent(true);
+
+        //     bool ret;
+        //     b.run(std::string("opencl CPU 7p Laplace, N = ").append(std::to_string(N)).c_str(), [&]
+        //           { ret = p.init(); });
+        //     REQUIRE(ret);
+        // }
+
+        // if (cpuAvailable)
+        // {
+        //     auto v = std::make_shared<mgcl::Cuboid>(m, n, o);
+        //     auto f = std::make_shared<mgcl::Cuboid>(m, n, o);
+        //     v->fillRandom(0, 10);
+        //     f->fillRandom(0, 10);
+
+        //     mgcl::Problem p(m, n, o, f, v);
+        //     p.setUseOpencl(true);
+        //     p.setDeviceType(CL_DEVICE_TYPE_CPU);
+        //     p.setSilent(true);
+
+        //     // this is equal to Problem::reuseOpenCL for our matter
+        //     REQUIRE(p.initOpenCL() == CL_SUCCESS);
+
+        //     bool ret;
+        //     b.run(std::string("opencl CPU reusing platform 7p Laplace, N = ").append(std::to_string(N)).c_str(), [&]
+        //           { ret = p.init(); });
+        //     REQUIRE(ret);
+        // }
     }
 }
---
+++ benchmarks/example_galerkin.cpp
@@ -0,0 +1,114 @@
+
+#include <iostream>
+#include <memory>
+
+#include "../cuboid.hpp"
+#include "../mgcl.hpp"
+#include "../problem.hpp"
+#include "../test/test_utility.hpp"
+
+enum RUNS
+{
+    SEQ,
+    OCL,
+    SEQ_OCL
+};
+
+// arguments:
+// 1. arg: int, grid size. Default is 16
+// 2. arg: string, determines if seq, ocl or both shall be called. Valid values: seq, ocl, seq+ocl. Default is seq
+int main(int argc, char **argv)
+{
+    int N = 16;
+    RUNS runs = SEQ;
+
+    if (argc >= 2)
+        N = atoi(argv[1]);
+
+    if (argc >= 3)
+    {
+        if (std::string(argv[2]) == "seq")
+            runs = SEQ;
+        else if (std::string(argv[2]) == "ocl")
+            runs = OCL;
+        else if (std::string(argv[2]) == "seq+ocl")
+            runs = SEQ_OCL;
+    }
+
+    std::cout << "Running with parameters:" << std::endl;
+    std::cout << "  N = " << N << std::endl;
+
+    int m = N;
+    int n = N;
+    int o = N;
+
+    // Problem parameters
+    double tol = 1e-20;
+    int nu1 = 2;
+    int nu2 = 2;
+    double omega = 0.8;
+    int maxIterVCycles = 30;
+
+    auto f = std::make_shared<mgcl::Cuboid>(m, n, o);
+    f->fillRandom(0, 10);
+
+    if (runs == SEQ || runs == SEQ_OCL)
+    {
+        std::cout << "Initializing mgcl seq with varying stencil..." << std::endl;
+        auto v = std::make_shared<mgcl::Cuboid>(m, n, o);
+
+        mgcl::Problem p(m, n, o, f, v);
+        p.setMaxiterVcycles(maxIterVCycles);
+        p.setIgnoreTol(true);
+        p.setSilent(true);
+        p.setNu1(nu1);
+        p.setNu2(nu2);
+        p.setOmega(omega);
+
+        p.setStencilType(mgcl::MGCL_VARYING_7POINT);
+        auto &s = *p.getStencilValues();
+
+        // fill with 7-point Laplace
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                for (int k = 0; k < o; k++)
+                {
+                    // 7-point Laplace
+                    s[i][j][k][0][1][1] = 1;
+                    s[i][j][k][1][0][1] = 1;
+                    s[i][j][k][1][1][0] = 1;
+                    s[i][j][k][1][1][1] = -6;
+                    s[i][j][k][1][1][2] = 1;
+                    s[i][j][k][1][2][1] = 1;
+                    s[i][j][k][2][1][1] = 1;
+                }
+
+        p.init();
+
+        // p.solveSeq();
+    }
+
+    if ((runs == OCL || runs == SEQ_OCL) && mgcl_test::TestUtility::deviceAvailable("", CL_DEVICE_TYPE_GPU))
+    {
+        std::cout << "Initializing mgcl OpenCL gpu..." << std::endl;
+        auto v = std::make_shared<mgcl::Cuboid>(m, n, o);
+
+        mgcl::Problem p(m, n, o, f, v);
+        p.setMaxiterVcycles(maxIterVCycles);
+        p.setIgnoreTol(true);
+        p.setUseOpencl(true);
+        p.setDeviceType(CL_DEVICE_TYPE_GPU);
+        p.setSilent(true);
+        p.setNu1(nu1);
+        p.setNu2(nu2);
+        p.setOmega(omega);
+
+        if (mgcl_test::TestUtility::deviceAvailable("Quadro", p.getDeviceType()))
+            p.setDeviceName("Quadro");
+
+        p.init();
+        // p.solve();
+    }
+
+    return 0;
+}
