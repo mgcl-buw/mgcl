8a82a651cbf0ff269633a34303004d26ec9868b5
Simon Hoffmann <shoffmann@uni-wuppertal.de>
2022-12-02 10:22:31 +0100

    galerkin test Laplace vs rediscretization

--- test/test_galerkin.cpp
+++ test/test_galerkin.cpp
@@ -9,7 +9,7 @@
 #include "../multigrid_engine.hpp"
 #include "../stencil.hpp"
 
-TEST_CASE("galerkin Laplace")
+TEST_CASE("galerkin Laplace vs Matrix")
 {
     int m = GENERATE(2, 4, 8);
     int n = GENERATE(2, 4, 8);
@@ -47,7 +47,7 @@ TEST_CASE("galerkin Laplace")
     CHECK(a2hm == a2h);
 }
 
-TEST_CASE("galerkin random values periodic")
+TEST_CASE("galerkin random values periodic vs Matrix")
 {
     int m = GENERATE(2, 4, 8);
     int n = GENERATE(2, 4, 8);
@@ -117,4 +117,80 @@ TEST_CASE("galerkin multiple levels random values periodic")
         // a_2h is a_h for next level
         a_h = std::move(a_2h);
     }
+}
+
+// If the fine grid operator is symmetric, the coarse grid operator shall be too (see "A Multigrid tutorial")
+TEST_CASE("galerkin symmetric")
+{
+    int m = GENERATE(2, 4, 8);
+    int n = GENERATE(2, 4, 8);
+    int o = GENERATE(2, 4, 8);
+
+    // Fill varying stencil on fine grid with 7p Laplace
+    mgcl::VaryingStencil3x3x3 a_h(m, n, o, 2, 2, 2);
+    for (int i = 0; i < m + 4; i++)
+        for (int j = 0; j < n + 4; j++)
+            for (int k = 0; k < o + 4; k++)
+            {
+                // 7-point Laplace
+                a_h[i][j][k][0][1][1] = 1;
+                a_h[i][j][k][1][0][1] = 1;
+                a_h[i][j][k][1][1][0] = 1;
+                a_h[i][j][k][1][1][1] = -6;
+                a_h[i][j][k][1][1][2] = 1;
+                a_h[i][j][k][1][2][1] = 1;
+                a_h[i][j][k][2][1][1] = 1;
+            }
+
+    auto a_2h = mgcl::MultigridEngine::galerkin(a_h);
+    auto a2hm = mgcl_test::Matrix2d::fromVaryingStencil(a_2h, true);
+
+    for (int i = 0; i < a2hm.getM(); i++)
+        for (int j = 0; j < a2hm.getN(); j++)
+        {
+            REQUIRE(a2hm[i][j] == a2hm[j][i]);
+        }
+}
+
+// For the Laplace operator the Galerkin coarse grid operator shall be equal to the rediscretization of the Laplace
+// operator on the coarse grid (using 2nd order differences, see "A Multigrid Tutorial", p. 79).
+TEST_CASE("galerkin Laplace rediscretized")
+{
+    int m = 8; // GENERATE(2, 4, 8);
+    int n = 8; // GENERATE(2, 4, 8);
+    int o = 8; // GENERATE(2, 4, 8);
+    double h = 1.0 / (double)m;
+    double h2inv = static_cast<double>(m * m);
+
+    // Fill varying stencil on fine grid with 7p Laplace
+    mgcl::VaryingStencil3x3x3 a_h(m, n, o, 2, 2, 2);
+    for (int i = 0; i < m + 4; i++)
+        for (int j = 0; j < n + 4; j++)
+            for (int k = 0; k < o + 4; k++)
+            {
+                // 7-point Laplace
+                a_h[i][j][k][0][1][1] = h2inv * 1.0;
+                a_h[i][j][k][1][0][1] = h2inv * 1.0;
+                a_h[i][j][k][1][1][0] = h2inv * 1.0;
+                a_h[i][j][k][1][1][1] = h2inv * -6.0;
+                a_h[i][j][k][1][1][2] = h2inv * 1.0;
+                a_h[i][j][k][1][2][1] = h2inv * 1.0;
+                a_h[i][j][k][2][1][1] = h2inv * 1.0;
+            }
+
+    auto a_2h = mgcl::MultigridEngine::galerkin(a_h);
+    h2inv = static_cast<double>(a_2h.getDim1() * a_2h.getDim1());
+
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            for (int k = 0; k < o; k++)
+            {
+                REQUIRE(a_2h[i][j][k][0][1][1] == h2inv * 1.0);
+                REQUIRE(a_2h[i][j][k][1][0][1] == h2inv * 1.0);
+                REQUIRE(a_2h[i][j][k][1][1][0] == h2inv * 1.0);
+                REQUIRE(a_2h[i][j][k][1][1][1] == h2inv * -6.0);
+                REQUIRE(a_2h[i][j][k][1][1][2] == h2inv * 1.0);
+                REQUIRE(a_2h[i][j][k][1][2][1] == h2inv * 1.0);
+                REQUIRE(a_2h[i][j][k][2][1][1] == h2inv * 1.0);
+            }
 }
