33688373c5a7c21e2566ffd6f5180fe4b1e171c2
Simon Hoffmann <shoffmann@uni-wuppertal.de>
2023-01-31 13:10:04 +0100

    clean up jacobi test (still not working)

--- test/test_jacobi.cpp
+++ test/test_jacobi.cpp
@@ -106,65 +106,82 @@ TEST_CASE("jacobi GPU varying stencil")
     int m = 8;
     int n = 8;
     int o = 8;
-    int ghosts_m = 1;
-    int ghosts_n = 1;
-    int ghosts_o = 1;
-    int mgh = m + 2 * ghosts_m;
-    int ngh = n + 2 * ghosts_n;
-    int ogh = o + 2 * ghosts_o;
     double omega = 0.8;
     int maxiter = 5;
 
-    // make sure to actually set grid size to 16^3 when using these values
-    // auto v_in = mgcl_test::test_jacobi::inputV16();
-    // auto f_in = mgcl_test::test_jacobi::inputF16();
-    // auto r_in = mgcl_test::test_jacobi::inputR16();
-    auto v_in = std::make_shared<mgcl::Cuboid>(m, n, o, ghosts_m, ghosts_n, ghosts_o);
-    auto f_in = std::make_shared<mgcl::Cuboid>(m, n, o, ghosts_m, ghosts_n, ghosts_o);
-    auto r_in = std::make_shared<mgcl::Cuboid>(m, n, o, ghosts_m, ghosts_n, ghosts_o);
+    auto v_in = std::make_shared<mgcl::Cuboid>(m, n, o, 1, 1, 1);
+    auto f_in = std::make_shared<mgcl::Cuboid>(m, n, o, 1, 1, 1);
+    auto r_in = std::make_shared<mgcl::Cuboid>(m, n, o, 1, 1, 1);
     f_in->fillRandomInt();
 
-    auto p = std::make_shared<mgcl::Problem>(m, n, o);
-    p->setResidualNorm(mgcl::MGCL_L2);
-    p->setGhosts(1);
-    p->setOmega(omega);
-    p->setDeviceType(deviceType);
-    p->setV(v_in);
-    p->setF(f_in);
+    // init sequential Problem
+    auto p_seq = std::make_shared<mgcl::Problem>(m, n, o);
+    p_seq->setResidualNorm(mgcl::MGCL_L2);
+    p_seq->setGhosts(1);
+    p_seq->setOmega(omega);
+    p_seq->setDeviceType(deviceType);
+    p_seq->setV(v_in);
+    p_seq->setF(f_in);
 
-    p->setStencilType(mgcl::MGCL_VARYING_27POINT);
-    auto &sv = p->getStencilValues();
+    p_seq->setStencilType(mgcl::MGCL_VARYING_27POINT);
+    auto &sv = p_seq->getStencilValues();
     sv->fillRandomInt();
 
-    p->init();
-    auto &level = p->getLevelAt(0);
-    // auto &sv = level.getStencilValues();
-    REQUIRE(level.getStencilValues().get() == sv.get());
+    p_seq->init();
+    auto &level0_seq = p_seq->getLevelAt(0);
+    REQUIRE(level0_seq.getStencilValues().get() == sv.get());
 
-    mgcl_test::TestUtility tu(p);
-    cl_mem d_in_f = tu.createOpenCLBuffer(*f_in);
-    cl_mem d_in_v = tu.createOpenCLBuffer(*v_in);
-    cl_mem d_in_v_out = tu.createOpenCLBuffer(*v_in);
-    cl_mem d_in_r = tu.createOpenCLBuffer(*r_in);
-    auto d_in_sv = std::make_shared<mgcl::VaryingStencilGpu>(sv->getDim1(), sv->getDim2(), sv->getDim3(),
-                                                             sv->getDim4(), sv->getGhostsDim1(),
-                                                             p->getContext(), p->getCommands());
-    d_in_sv->fill(*sv, p->getCommands());
+    // init OpenCL problem
+    auto p_gpu = std::make_shared<mgcl::Problem>(m, n, o);
+    p_gpu->setResidualNorm(mgcl::MGCL_L2);
+    p_gpu->setGhosts(1);
+    p_gpu->setOmega(omega);
+    p_gpu->setDeviceType(deviceType);
+    p_gpu->setV(v_in);
+    p_gpu->setF(f_in);
+    p_gpu->setUseOpencl(true);
 
-    level.setDF(d_in_f);
-    level.setDVIn(d_in_v);
-    level.setDVOut(d_in_v_out);
-    level.setDR(d_in_r);
-    level.setStencilValuesGpu(d_in_sv);
+    p_gpu->setStencilType(mgcl::MGCL_VARYING_27POINT);
+    auto &sv_gpu = p_gpu->getStencilValues();
 
-    double res_gpu = mgcl::MultigridEngine::jacobi(*p, level, maxiter, 1);
+    REQUIRE(sv->field1d().size() == sv_gpu->field1d().size());
+    for (int i = 0; i < sv->field1d().size(); i++)
+        sv_gpu->field1d()[i] = sv->field1d()[i];
+
+    p_gpu->init();
+    auto &level0_gpu = p_gpu->getLevelAt(0);
+    REQUIRE(level0_gpu.getStencilValuesGpu() != nullptr);
+
+    auto &v_in_lv0 = level0_seq.getV();
+    auto &f_in_lv0 = level0_seq.getF();
+    auto &r_in_lv0 = level0_seq.getR();
+    auto &sv_in_lv0 = level0_seq.getStencilValues();
+
+    mgcl_test::TestUtility tu(p_gpu);
+
+    // make sure input is equal
+    auto dv_in_lv0 = level0_gpu.getDVIn();
+    auto dr_in_lv0 = level0_gpu.getDR();
+    auto df_in_lv0 = level0_gpu.getDF();
+    auto dsv_in_lv0 = level0_gpu.getStencilValuesGpu();
+    auto c_r_in = tu.readOpenCLBuffer(dr_in_lv0, m, n, o, 1, 1, 1);
+    auto c_v_in = tu.readOpenCLBuffer(dv_in_lv0, m, n, o, 1, 1, 1);
+    auto c_f_in = tu.readOpenCLBuffer(df_in_lv0, m, n, o, 1, 1, 1);
+    auto c_sv_in = dsv_in_lv0->read<3>(tu.getCommands());
     tu.finish();
+    REQUIRE(c_r_in->isEqual(r_in_lv0));
+    REQUIRE(c_v_in->isEqual(v_in_lv0));
+    REQUIRE(c_f_in->isEqual(f_in_lv0));
+    REQUIRE(c_sv_in.isEqual(*sv_in_lv0));
 
-    double res_seq = mgcl::MultigridEngine::jacobiSeq(*v_in, *f_in, *r_in, omega, maxiter, mgcl::MGCL_L2,
-                                                      mgcl::MGCL_VARYING_27POINT, 1, *sv, 1);
+    double res_gpu = mgcl::MultigridEngine::jacobi(*p_gpu, level0_gpu, maxiter, 1);
+    tu.finish();
+    double res_seq = mgcl::MultigridEngine::jacobiSeq(v_in_lv0, f_in_lv0, r_in_lv0, omega, maxiter, mgcl::MGCL_L2,
+                                                      mgcl::MGCL_VARYING_27POINT, 1, *sv_in_lv0, 1);
 
-    auto c_r_out = tu.readOpenCLBuffer(d_in_r, m, n, o, ghosts_m, ghosts_n, ghosts_o);
-    auto c_v_out = tu.readOpenCLBuffer(d_in_v, m, n, o, ghosts_m, ghosts_n, ghosts_o);
+    auto c_r_out = tu.readOpenCLBuffer(dr_in_lv0, m, n, o, 1, 1, 1);
+    auto c_v_out = tu.readOpenCLBuffer(dv_in_lv0, m, n, o, 1, 1, 1);
+    tu.finish();
 
     REQUIRE(fabs(res_seq - res_gpu) < 1e-13);
     REQUIRE(c_r_out->isEqual(*r_in));
