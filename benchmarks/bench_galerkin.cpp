b64537134cb3759c95d531a3806fb8432995437c
Simon Hoffmann <shoffmann@uni-wuppertal.de>
2023-03-31 14:23:49 +0200

    better benchmarking for init vs solve (measuring overhead)

--- benchmarks/bench_galerkin.cpp
+++ benchmarks/bench_galerkin.cpp
@@ -5,6 +5,7 @@
 
 #include <chrono>
 #include <fstream>
+#include <functional> // for function
 #include <iostream>
 #include <vector>
 using namespace std::chrono_literals;
@@ -16,18 +17,21 @@ using namespace std::chrono_literals;
 
 TEST_CASE("galerkin init vs solve", "[console][galerkinInitVsSolve]")
 {
-    // int N = GENERATE(16, 32, 64);
-    int N = 32;
+    // int N = GENERATE(8, 16, 32, 64);
+    int N = 64;
     int m = N;
     int n = N;
     int o = N;
 
+    int vcycleIters = 30;
+
     ankerl::nanobench::Bench b;
-    b.timeUnit(1ms, "ms")
+    b.timeUnit(1ns, "ns")
         // .epochs(1)
         // .epochIterations(1)
         .minEpochTime(100ms)
         .relative(false);
+    // .warmup(1); // especially for init OpenCL environment
 
     bool gpuAvailable = mgcl_test::TestUtility::deviceAvailable("", CL_DEVICE_TYPE_GPU);
     // bool cpuAvailable = tu->deviceAvailable("", CL_DEVICE_TYPE_CPU);
@@ -41,21 +45,37 @@ TEST_CASE("galerkin init vs solve", "[console][galerkinInitVsSolve]")
             v->fillRandom(0, 10);
             f->fillRandom(0, 10);
 
-            mgcl::Problem p(m, n, o, f, v);
-            p.setSilent(true);
-
             std::string name = std::string("seq fixed 7p Laplace, N = ")
                                    .append(std::to_string(N))
                                    .append(", iters: ")
-                                   .append(std::to_string(p.getMaxiterVcycles()));
+                                   .append(std::to_string(vcycleIters));
 
-            bool ret;
+            b.run(std::string(name).append(", overhead").c_str(), [&]
+                  {
+                      auto p = new mgcl::Problem(m, n, o, f, v);
+                      p->setSilent(true);
+                      p->setMaxiterVcycles(vcycleIters);
+                      // p.init();
+                      delete p; //
+                  });
+
             b.run(std::string(name).append(", init").c_str(), [&]
-                  { ret = p.init(); });
-            REQUIRE(ret);
+                  {
+                      auto p = new mgcl::Problem(m, n, o, f, v);
+                      p->setSilent(true);
+                      p->setMaxiterVcycles(vcycleIters);
+                      p->init();
+                      delete p; //
+                  });
 
             b.run(std::string(name).append(", init+solve").c_str(), [&]
-                  { p.solve(); });
+                  {
+                      auto p = new mgcl::Problem(m, n, o, f, v);
+                      p->setSilent(true);
+                      p->setMaxiterVcycles(vcycleIters);
+                      p->solve();
+                      delete p; //
+                  });
         }
 
         {
@@ -65,125 +85,168 @@ TEST_CASE("galerkin init vs solve", "[console][galerkinInitVsSolve]")
             v->fillRandom(0, 10);
             f->fillRandom(0, 10);
 
-            mgcl::Problem p(m, n, o, f, v);
-            p.setSilent(true);
-            p.setStencilType(mgcl::MGCL_VARYING);
-            auto &s = *p.getStencilValues();
+            // mgcl::Problem p(m, n, o, f, v);
+            // p.setSilent(true);
+            // p.setStencilType(mgcl::MGCL_VARYING);
+            // auto &s = *p.getStencilValues();
 
-            // Fill with 7-point Laplace, which is also used by the other two Sections in this test case
-            for (int i = 0; i < N; i++)
-                for (int j = 0; j < N; j++)
-                    for (int k = 0; k < N; k++)
-                    {
-                        // 7-point Laplace
-                        s[i][j][k][0][1][1] = 1;
-                        s[i][j][k][1][0][1] = 1;
-                        s[i][j][k][1][1][0] = 1;
-                        s[i][j][k][1][1][1] = -6;
-                        s[i][j][k][1][1][2] = 1;
-                        s[i][j][k][1][2][1] = 1;
-                        s[i][j][k][2][1][1] = 1;
-                    }
+            std::function<void(mgcl::VaryingStencil3x3x3 &)> fillStencil = [N](mgcl::VaryingStencil3x3x3 &s)
+            {
+                // Fill with 7-point Laplace, which is also used by the other two Sections in this test case
+                for (int i = 0; i < N; i++)
+                    for (int j = 0; j < N; j++)
+                        for (int k = 0; k < N; k++)
+                        {
+                            // 7-point Laplace
+                            s[i][j][k][0][1][1] = 1;
+                            s[i][j][k][1][0][1] = 1;
+                            s[i][j][k][1][1][0] = 1;
+                            s[i][j][k][1][1][1] = -6;
+                            s[i][j][k][1][1][2] = 1;
+                            s[i][j][k][1][2][1] = 1;
+                            s[i][j][k][2][1][1] = 1;
+                        }
+            };
 
             std::string name = std::string("seq varying 7p Laplace, N = ")
                                    .append(std::to_string(N))
                                    .append(", iters: ")
-                                   .append(std::to_string(p.getMaxiterVcycles()));
+                                   .append(std::to_string(vcycleIters));
+
+            b.run(std::string(name).append(", overhead").c_str(), [&]
+                  {
+                      auto p = new mgcl::Problem(m, n, o, f, v);
+                      p->setSilent(true);
+                      p->setMaxiterVcycles(vcycleIters);
+                      p->setStencilType(mgcl::MGCL_VARYING);
+                      fillStencil(*p->getStencilValues());
+
+                      // p.init();
+                      delete p; //
+                  });
 
             if (N >= 32)
                 b.epochs(1).epochIterations(1);
 
-            bool ret;
             b.run(std::string(name).append(", init").c_str(), [&]
-                  { ret = p.init(); });
-            REQUIRE(ret);
+                  {
+                      auto p = new mgcl::Problem(m, n, o, f, v);
+                      p->setSilent(true);
+                      p->setMaxiterVcycles(vcycleIters);
+                      p->setStencilType(mgcl::MGCL_VARYING);
+                      fillStencil(*p->getStencilValues());
+
+                      p->init();
+                      delete p; //
+                  });
 
             b.run(std::string(name).append(", init+solve").c_str(), [&]
-                  { p.solve(); });
+                  {
+                      auto p = new mgcl::Problem(m, n, o, f, v);
+                      p->setSilent(true);
+                      p->setMaxiterVcycles(vcycleIters);
+                      p->setStencilType(mgcl::MGCL_VARYING);
+                      fillStencil(*p->getStencilValues());
+
+                      p->solve();
+                      delete p; //
+                  });
 
             if (N >= 32)
                 b.epochs(11).epochIterations(0);
         }
 
-        {
-            // OpenCL fixed Laplace stencil
-            auto v = std::make_shared<mgcl::Cuboid>(m, n, o);
-            auto f = std::make_shared<mgcl::Cuboid>(m, n, o);
-            v->fillRandom(0, 10);
-            f->fillRandom(0, 10);
+        // if (gpuAvailable)
+        // {
+        //     // OpenCL fixed Laplace stencil
+        //     auto v = std::make_shared<mgcl::Cuboid>(m, n, o);
+        //     auto f = std::make_shared<mgcl::Cuboid>(m, n, o);
+        //     v->fillRandom(0, 10);
+        //     f->fillRandom(0, 10);
 
-            mgcl::Problem p(m, n, o, f, v);
-            p.setSilent(true);
-            p.setUseOpencl(true);
+        //     auto p = new mgcl::Problem(m, n, o, f, v);
+        //     p->setSilent(true);
+        //     p->setUseOpencl(true);
 
-            std::string name = std::string("ocl fixed 7p Laplace, N = ")
-                                   .append(std::to_string(N))
-                                   .append(", iters: ")
-                                   .append(std::to_string(p.getMaxiterVcycles()));
+        //     std::string name = std::string("ocl fixed 7p Laplace, N = ")
+        //                            .append(std::to_string(N))
+        //                            .append(", iters: ")
+        //                            .append(std::to_string(p->getMaxiterVcycles()));
 
-            bool ret;
-            b.run(std::string(name).append(", init").c_str(), [&]
-                  { 
-                    ret = p.init();
-                    clFinish(p.getCommands()); });
-            REQUIRE(ret);
+        //     bool ret;
+        //     b.run(std::string(name).append(", init").c_str(), [&]
+        //           {
+        //               ret = p->init();
+        //               clFinish(p->getCommands());
+        //               p->reset();
+        //               // delete p;
+        //           });
+        //     REQUIRE(ret);
 
-            b.run(std::string(name).append(", init+solve").c_str(), [&]
-                  { 
-                    p.solve();
-                    clFinish(p.getCommands()); });
-        }
+        //     p = new mgcl::Problem(m, n, o, f, v);
+        //     p->setSilent(true);
+        //     p->setUseOpencl(true);
 
-        {
-            // OpenCL varying stencil (Galerkin)
-            auto v = std::make_shared<mgcl::Cuboid>(m, n, o);
-            auto f = std::make_shared<mgcl::Cuboid>(m, n, o);
-            v->fillRandom(0, 10);
-            f->fillRandom(0, 10);
+        //     b.run(std::string(name).append(", init+solve").c_str(), [&]
+        //           {
+        //               p->solve();
+        //               clFinish(p->getCommands());
+        //               p->reset();
+        //               // delete p;
+        //           });
+        // }
 
-            mgcl::Problem p(m, n, o, f, v);
-            p.setSilent(true);
-            p.setStencilType(mgcl::MGCL_VARYING);
-            p.setUseOpencl(true);
-            auto &s = *p.getStencilValues();
+        // if (gpuAvailable)
+        // {
+        //     // OpenCL varying stencil (Galerkin)
+        //     auto v = std::make_shared<mgcl::Cuboid>(m, n, o);
+        //     auto f = std::make_shared<mgcl::Cuboid>(m, n, o);
+        //     v->fillRandom(0, 10);
+        //     f->fillRandom(0, 10);
 
-            // Fill with 7-point Laplace, which is also used by the other two Sections in this test case
-            for (int i = 0; i < N; i++)
-                for (int j = 0; j < N; j++)
-                    for (int k = 0; k < N; k++)
-                    {
-                        // 7-point Laplace
-                        s[i][j][k][0][1][1] = 1;
-                        s[i][j][k][1][0][1] = 1;
-                        s[i][j][k][1][1][0] = 1;
-                        s[i][j][k][1][1][1] = -6;
-                        s[i][j][k][1][1][2] = 1;
-                        s[i][j][k][1][2][1] = 1;
-                        s[i][j][k][2][1][1] = 1;
-                    }
+        //     mgcl::Problem p(m, n, o, f, v);
+        //     p.setSilent(true);
+        //     p.setStencilType(mgcl::MGCL_VARYING);
+        //     p.setUseOpencl(true);
+        //     auto &s = *p.getStencilValues();
 
-            std::string name = std::string("ocl varying 7p Laplace, N = ")
-                                   .append(std::to_string(N))
-                                   .append(", iters: ")
-                                   .append(std::to_string(p.getMaxiterVcycles()));
+        //     // Fill with 7-point Laplace, which is also used by the other two Sections in this test case
+        //     for (int i = 0; i < N; i++)
+        //         for (int j = 0; j < N; j++)
+        //             for (int k = 0; k < N; k++)
+        //             {
+        //                 // 7-point Laplace
+        //                 s[i][j][k][0][1][1] = 1;
+        //                 s[i][j][k][1][0][1] = 1;
+        //                 s[i][j][k][1][1][0] = 1;
+        //                 s[i][j][k][1][1][1] = -6;
+        //                 s[i][j][k][1][1][2] = 1;
+        //                 s[i][j][k][1][2][1] = 1;
+        //                 s[i][j][k][2][1][1] = 1;
+        //             }
 
-            if (N >= 32)
-                b.epochs(1).epochIterations(1);
+        //     std::string name = std::string("ocl varying 7p Laplace, N = ")
+        //                            .append(std::to_string(N))
+        //                            .append(", iters: ")
+        //                            .append(std::to_string(p.getMaxiterVcycles()));
 
-            bool ret;
-            b.run(std::string(name).append(", init").c_str(), [&]
-                  { 
-                    ret = p.init(); 
-                    clFinish(p.getCommands()); });
-            REQUIRE(ret);
+        //     // if (N >= 32)
+        //     //     b.epochs(1).epochIterations(1);
 
-            b.run(std::string(name).append(", init+solve").c_str(), [&]
-                  { 
-                    p.solve(); 
-                    clFinish(p.getCommands()); });
+        //     bool ret;
+        //     b.run(std::string(name).append(", init").c_str(), [&]
+        //           {
+        //             ret = p.init();
+        //             clFinish(p.getCommands()); });
+        //     REQUIRE(ret);
 
-            if (N >= 32)
-                b.epochs(11).epochIterations(0);
-        }
+        //     b.run(std::string(name).append(", init+solve").c_str(), [&]
+        //           {
+        //             p.solve();
+        //             clFinish(p.getCommands()); });
+
+        //     // if (N >= 32)
+        //     //     b.epochs(11).epochIterations(0);
+        // }
     }
 }
