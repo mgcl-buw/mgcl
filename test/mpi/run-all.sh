78866a4b18327dd00f2825d0f399bd80e9d73428
Simon Hoffmann <shoffmann@uni-wuppertal.de>
2023-09-04 12:16:25 +0200

    enhanced run-all mpi tests script

--- test/mpi/run-all.sh
+++ test/mpi/run-all.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-# Cancel script on first failure
-set -e
-
 # This script runs all tests in the MPI test folder.
 # Run from build/test directory.
 # New tests must be added manually.
+
+# Cancel script on first failure
+set -e
+
+# Function that runs a test, captures the output of it and prints it only when
+# the exit code is not 0. Arguments are the test command, so call it e.g. like so:
+# run_test mpiexec --oversubscribe -n 4 "$exe" "MPI Problem::calculateAndSetMaxLevel (4 processes)"
+run_test() {
+  echo "Running mpiexec $* ..."
+  cmdout=$(mpiexec "$@")
+  ret=$?
+  if ((ret)); then
+    echo >&2 "Test returned code $ret:" 
+    echo >&2 "$cmdout"
+  fi
+}
 
 TEST_ALL=true
 TEST_PROBLEM=false
@@ -63,11 +76,11 @@ if [ "$TEST_PROBLEM" = true ] ; then
     echo "Run Tests from test_mpi_problem.cpp ..."
     echo "#######################"
 
-    $exe "MPI Problem::setMpiComm"
-    $exe "MPI Problem::calculateAndSetMaxLevel (1 process)"
-    mpiexec --oversubscribe -n 4 "$exe" "MPI Problem::calculateAndSetMaxLevel (4 processes)"
-    $exe "MPI Problem::calculateAndSetMpiLevelThreshold valid (1 process)"
-    $exe "MPI Problem::calculateAndSetMpiLevelThreshold throwing (1 process)"
+    run_test -n 1 "$exe" "MPI Problem::setMpiComm"
+    run_test -n 1 "$exe" "MPI Problem::calculateAndSetMaxLevel (1 process)"
+    run_test --oversubscribe -n 4 "$exe" "MPI Problem::calculateAndSetMaxLevel (4 processes)"
+    run_test -n 1 "$exe" "MPI Problem::calculateAndSetMpiLevelThreshold valid (1 process)"
+    run_test -n 1 "$exe" "MPI Problem::calculateAndSetMpiLevelThreshold throwing (1 process)"
 fi
 
 if [ "$TEST_LEVEL" = true ] ; then
@@ -75,10 +88,10 @@ if [ "$TEST_LEVEL" = true ] ; then
     echo "Run Tests from test_mpi_level.cpp ..."
     echo "#######################"
 
-    $exe "Level::initMpiData (1 process)"
-    mpiexec --oversubscribe -n 2 "$exe" "Level::initMpiData (2 processes)"
-    mpiexec --oversubscribe -n 8 "$exe" "Level::initMpiData (8 processes)"
-    mpiexec --oversubscribe -n 24 "$exe" "Level::initMpiData (24 processes)"
+    run_test -n 1 "$exe" "Level::initMpiData (1 process)"
+    run_test --oversubscribe -n 2 "$exe" "Level::initMpiData (2 processes)"
+    run_test --oversubscribe -n 8 "$exe" "Level::initMpiData (8 processes)"
+    run_test --oversubscribe -n 24 "$exe" "Level::initMpiData (24 processes)"
 fi
 
 if [ "$TEST_GHOSTS" = true ] ; then
@@ -86,9 +99,9 @@ if [ "$TEST_GHOSTS" = true ] ; then
     echo "Run Tests from test_mpi_ghosts.cpp ..."
     echo "#######################"
 
-    $exe "MPI updateGhostsSeq (1 process)"
-    mpiexec --oversubscribe -n 8 "$exe" "MPI updateGhostsSeq (n processes)"
-    mpiexec --oversubscribe -n 8 "$exe" "MPI updateGhosts ocl (n processes)"
+    run_test -n 1 "$exe" "MPI updateGhostsSeq (1 process)"
+    run_test --oversubscribe -n 8 "$exe" "MPI updateGhostsSeq (n processes)"
+    run_test --oversubscribe -n 8 "$exe" "MPI updateGhosts ocl (n processes)"
 fi
 
 if [ "$TEST_JACOBI" = true ] ; then
@@ -96,8 +109,8 @@ if [ "$TEST_JACOBI" = true ] ; then
     echo "Run Tests from test_mpi_jacobi.cpp ..."
     echo "#######################"
 
-    mpiexec --oversubscribe -n 8 "$exe" "MPI jacobiSeq (n processes)"
-    mpiexec --oversubscribe -n 8 "$exe" "MPI jacobi ocl (n processes)"
+    run_test --oversubscribe -n 8 "$exe" "MPI jacobiSeq (n processes)"
+    run_test --oversubscribe -n 8 "$exe" "MPI jacobi ocl (n processes)"
 fi
 
 if [ "$TEST_UTIL" = true ] ; then
@@ -105,9 +118,9 @@ if [ "$TEST_UTIL" = true ] ; then
     echo "Run Tests from test_mpi_util.cpp ..."
     echo "#######################"
 
-    mpiexec --oversubscribe -n 4 "$exe" "MPI_Gatherv src dest different"
-    mpiexec --oversubscribe -n 4 "$exe" "MPI_Gatherv src dest same"
-    mpiexec --oversubscribe -n 4 "$exe" "mpi_util::scatter src dest same"
+    run_test --oversubscribe -n 4 "$exe" "MPI_Gatherv src dest different"
+    run_test --oversubscribe -n 4 "$exe" "MPI_Gatherv src dest same"
+    run_test --oversubscribe -n 4 "$exe" "mpi_util::scatter src dest same"
 fi
 
 if [ "$TEST_VCYCLE" = true ] ; then
@@ -115,7 +128,9 @@ if [ "$TEST_VCYCLE" = true ] ; then
     echo "Run Tests from test_mpi_vcycle.cpp ..."
     echo "#######################"
 
-    mpiexec --oversubscribe -n 1 "$exe" "MPI_vcycle_immediate_gather_scatter"
-    mpiexec --oversubscribe -n 2 "$exe" "MPI_vcycle_immediate_gather_scatter"
+    run_test --oversubscribe -n 1 "$exe" "MPI_vcycle_immediate_gather_scatter"
+    run_test --oversubscribe -n 2 "$exe" "MPI_vcycle_immediate_gather_scatter"
 fi
+
+echo "Done. All good!"
 
