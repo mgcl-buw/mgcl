b4cbd14c0aa248afe6843fd7ac86e1fd4e3520e3
Simon Hoffmann <shoffmann@uni-wuppertal.de>
2023-09-21 13:14:18 +0200

    added mpi vcycle multiple jacobi iters test

--- test/mpi/run-all.sh
+++ test/mpi/run-all.sh
@@ -176,6 +176,7 @@ if [ "$TEST_VCYCLE" = true ] || [ "$TEST_ALL" = true ] ; then
     run_test --oversubscribe -n 4 "$exe" "MPI_vcycle_GPU_threshold_eq_1_Varying27p"
     run_test --oversubscribe -n 1 "$exe" "MPI_vcycle_GPU_threshold_eq_2_Varying27p"
     run_test --oversubscribe -n 4 "$exe" "MPI_vcycle_GPU_threshold_eq_2_Varying27p"
+    run_test --oversubscribe -n 4 "$exe" "MPI_vcycle_GPU_threshold_eq_2_Varying27p_multiple_jacobi_iters"
 fi
 
 if [ "$TEST_STENCIL" = true ] || [ "$TEST_ALL" = true ] ; then
