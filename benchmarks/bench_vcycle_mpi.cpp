7d4868458976ca30f141221dcdcaaff590c17671
Simon Hoffmann <shoffmann@uni-wuppertal.de>
2023-09-25 11:09:56 +0200

    added vcycle bench for different jacobi iters w/o ghost update

--- benchmarks/bench_vcycle_mpi.cpp
+++ benchmarks/bench_vcycle_mpi.cpp
@@ -353,3 +353,121 @@ TEST_CASE("benchmark_vcycle_MPI_OCL_galerkin_thresholds")
         }
     }
 }
+
+// Benchmarks different jacobiItersPerKernel for the vcycle using MPI and OpenCL.
+// Only rank 0 will print the timings.
+// Run with e.g.: mpiexec -n 4 benchmarks benchmark_vcycle_MPI_OCL_galerkin_jacobi_iters
+TEST_CASE("benchmark_vcycle_MPI_OCL_galerkin_jacobi_iters")
+{
+    using std::min;
+
+    if (CLI_ARGS::grids.size() == 0)
+        throw "Need to specify at least one local grid size, e.g. using --grids 4,8,16";
+
+    // Check if mpi is initialized
+    int isInitialized = 0;
+    MPI_Initialized(&isInitialized);
+    REQUIRE(isInitialized);
+
+    MPI_Comm mpi_comm = MPI_COMM_WORLD;
+
+    // Check number of processes
+    int mpi_size = -1;
+    MPI_Comm_size(mpi_comm, &mpi_size);
+    // REQUIRE(mpi_size == 1);
+
+    int periodic = 1;
+
+    /* MPI variables */
+    int mpi_rank;
+    int mpi_dims[3] = {0, 0, 0};
+    int mpi_periods[3] = {periodic, periodic, periodic};
+    int mpi_coords[3];
+
+    /* Initialize cartesian process grid */
+    MPI_Comm_size(mpi_comm, &mpi_size);
+    MPI_Dims_create(mpi_size, 3, mpi_dims);
+    MPI_Cart_create(mpi_comm, 3, mpi_dims, mpi_periods, 1, &mpi_comm);
+    MPI_Comm_rank(mpi_comm, &mpi_rank);
+    MPI_Cart_coords(mpi_comm, mpi_rank, 3, mpi_coords);
+
+    double omega = 0.8;
+    mgcl::MGCL_RESIDUAL_NORM resnorm = mgcl::MGCL_L2;
+    mgcl::MGCL_STENCIL stencilType = mgcl::MGCL_VARYING;
+
+    if (mpi_rank == 0)
+        std::cout << "Problem parameters:" << std::endl
+                  << "  Omega: " << omega << std::endl
+                  << "  VCycle iterations: " << CLI_ARGS::vCycleIterations << std::endl
+                  << "  #procs: " << mpi_size << std::endl;
+
+    std::vector<int> itersPerKernel{1, 2, 3};
+    std::vector<int> nus{2, 3, 5, 10}; // nu1 and nu2
+
+    for (auto N : CLI_ARGS::grids)
+    {
+        int mglob = N * mpi_dims[0];
+        int nglob = N * mpi_dims[1];
+        int oglob = N * mpi_dims[2];
+        double h = 1.0 / (double)mglob;
+
+        for (auto nu12 : nus)
+        {
+            ankerl::nanobench::Bench bench;
+            bench.timeUnit(1ns, "ns")
+                .epochs(11)
+                .epochIterations(1)
+                // .minEpochTime(100ms)
+                .relative(true);
+
+            // disable output for non-root processes
+            if (mpi_rank > 0)
+                bench.output(nullptr);
+
+            for (auto ipk : itersPerKernel)
+            {
+                if (ipk > nu12)
+                    continue;
+
+                int ghin = 0;
+                auto v = std::make_shared<mgcl::Cuboid>(N, N, N, ghin, ghin, ghin);
+                auto f = std::make_shared<mgcl::Cuboid>(N, N, N, ghin, ghin, ghin);
+                v->fillRandom();
+                f->fillRandom();
+
+                mgcl::Problem pocl(N, N, N, v, f, mglob, nglob, oglob);
+                pocl.setSilent(true);
+                // pocl.setMpiMinGridPoints(mgp);
+                pocl.setOmega(omega);
+                pocl.setNu1(nu12);
+                pocl.setNu2(nu12);
+                pocl.setMaxiterVcycles(CLI_ARGS::vCycleIterations);
+                pocl.setGhostsIn(ghin);
+                pocl.setJacobiIterationsPerKernel(ipk);
+                pocl.setGhosts(ipk);
+                pocl.setStencilType(stencilType);
+                pocl.setResidualNorm(resnorm);
+                pocl.setMpiComm(mpi_comm);
+                pocl.setUseOpencl(true);
+                pocl.setDeviceType(CL_DEVICE_TYPE_GPU);
+                pocl.setReadResults(true);
+
+                auto& sv = pocl.getStencilValues();
+                sv->fill1dIndex(true);
+
+                std::string name = std::string("oclN")
+                                       .append(std::to_string(N))
+                                       .append("nu")
+                                       .append(std::to_string(nu12))
+                                       .append("ipk")
+                                       .append(std::to_string(ipk));
+
+                bench.run(std::string(name).c_str(), [&] { //
+                    pocl.solve();
+                    // tu.finish(); //
+                    MPI_Barrier(mpi_comm);
+                });
+            }
+        }
+    }
+}
