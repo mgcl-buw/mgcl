9a9df79a4cac76761c778f34c3f3aa055a26453f
Simon Hoffmann <shoffmann@uni-wuppertal.de>
2023-10-31 12:29:15 +0100

    added example for printing devices

---
+++ examples/example_print_devices.cpp
@@ -0,0 +1,45 @@
+
+#include <iostream>
+#include <memory>
+
+#include "mpi.h"
+
+#include "../src/mgcl/cuboid.hpp"
+#include "../src/mgcl/problem.hpp"
+
+/*
+ * This program creates a problem using OpenCL, which then prints the used devices.
+ * This is mainly useful for checking which process uses which device in a multi-device setup.
+ */
+int main(int argc, char** argv)
+{
+    MPI_Init(&argc, &argv);
+
+    int mpi_size;
+    int mpi_rank;
+
+    MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+
+    int N = 16;
+
+    auto f = std::make_shared<mgcl::Cuboid>(N, N, N);
+    auto v = std::make_shared<mgcl::Cuboid>(N, N, N);
+    mgcl::Problem p(N, N, N, f, v);
+    p.setUseOpencl(true);
+
+    for (int i = 0; i < mpi_size; i++)
+    {
+        MPI_Barrier(MPI_COMM_WORLD);
+        if (i == mpi_rank)
+        {
+            std::cerr << "on rank " << i << ": ";
+            p.getOpenCLHelper().init();
+            std::cout << std::flush;
+        }
+    }
+
+    MPI_Finalize();
+
+    return 0;
+}
