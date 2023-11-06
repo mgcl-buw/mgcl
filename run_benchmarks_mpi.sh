5e4e9a59ef21743b646c8bf0159decd32ea81d9d
Simon Hoffmann <shoffmann@uni-wuppertal.de>
2023-11-06 11:24:33 +0100

    updated slurm scripts

--- run_benchmarks_mpi.sh
+++ run_benchmarks_mpi.sh
@@ -4,11 +4,10 @@
 #SBATCH --partition=gpu
 #SBATCH --account=imacm_gpu
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
-#SBATCH --gpus 4
-#SBATCH --gres=gpu:4            # number of GPUs per node (gres=gpu:N)
-## #SBATCH --gpus-per-task 1
-#SBATCH --cpus-per-task 1
+
+#SBATCH --ntasks=2
+#SBATCH --gpus 2
+#SBATCH --gpus-per-task 1
 
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 
@@ -16,6 +15,6 @@ cd "$HOME"/projects/mgcl/build/benchmarks || exit
 
 # run e.g. with 
 # sbatch --chdir /beegfs/shoffmann/projects/mgcl/build/ run_benchmarks.sh [solve][console]
-mpiexec -n 1 "$HOME"/projects/mgcl/build/benchmarks/benchmarks "$@" > ~/output/"$current_time"-mgcl-bench.txt
+srun "$HOME"/projects/mgcl/build/benchmarks/benchmarks "$@" > ~/output/"$current_time"-mgcl-bench.txt
 
 cd "$HOME"/projects/mgcl || exit
--- run_benchmarks_mpi_seq.sh
+++ run_benchmarks_mpi_seq.sh
@@ -4,10 +4,10 @@
 #SBATCH --partition=gpu
 #SBATCH --account=imacm_gpu
 #SBATCH -N1
-#SBATCH -n32
+#SBATCH -n64
 #SBATCH --gpus 1
 # #SBATCH --gpus-per-task 1
-# #SBATCH --cpus-per-task 32
+# #SBATCH --cpus-per-task 64
 
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 
@@ -15,6 +15,6 @@ cd "$HOME"/projects/mgcl/build/benchmarks || exit
 
 # run e.g. with 
 # sbatch --chdir /beegfs/shoffmann/projects/mgcl/build/ run_benchmarks.sh [solve][console]
-mpiexec -n 32 "$HOME"/projects/mgcl/build/benchmarks/benchmarks "$@" > ~/output/"$current_time"-mgcl-bench-1-32.txt
+srun "$HOME"/projects/mgcl/build/benchmarks/benchmarks "$@" > ~/output/"$current_time"-mgcl-bench-seq-64.txt
 
 cd "$HOME"/projects/mgcl || exit
