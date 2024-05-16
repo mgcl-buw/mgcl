e2f3f5531cb7fbe990edcef3e02d866ff0034868
Simon Hoffmann <shoffmann@uni-wuppertal.de>
2024-05-16 10:18:49 +0200

    mpi ghost update test OK. Done with new ghost update

--- src/mgcl/ghostscl.cpp
+++ src/mgcl/ghostscl.cpp
@@ -441,6 +441,6 @@ namespace mgcl
                                    sbuf, rbuf, mpiData);
 
         // Paste planes back into the buffer.
-        d_buf.pasteGhostsFromBorderPlanes(p.getContext(), p.getCommands(), p.getProgram(), &d_tmp, rbuf_ptr.get());
+        d_buf.pasteGhostsFromBorderPlanes(p.getContext(), p.getCommands(), p.getProgram(), nullptr, rbuf_ptr.get());
     }
 }
