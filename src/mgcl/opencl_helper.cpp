fecf2137897702256900cd5e58b50fcfe8787da2
Simon Hoffmann <shoffmann@uni-wuppertal.de>
2024-05-16 11:03:25 +0200

    bugfix querying uuid, check availibilty correctly

--- src/mgcl/opencl_helper.cpp
+++ src/mgcl/opencl_helper.cpp
@@ -547,6 +547,7 @@ namespace mgcl
                               &uuid, nullptr);
         uuid_available = err == CL_SUCCESS;
         if (uuid_available)
+        {
             printf(", uuid: %02x%02x%02x%02x-"
                    "%02x%02x-"
                    "%02x%02x-"
@@ -557,10 +558,15 @@ namespace mgcl
                    uuid[7], uuid[8],
                    uuid[9], uuid[10],
                    uuid[11], uuid[12], uuid[13], uuid[14], uuid[15]);
-
+        }
         else
-#endif
+        {
+            err = CL_SUCCESS;
             printf("\n");
+        }
+#else
+        printf("\n");
+#endif
 
         return err;
     }
