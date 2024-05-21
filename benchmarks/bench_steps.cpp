f186e5d5930cb122d0d2528e8a9719cd4151c144
Simon Hoffmann <shoffmann@uni-wuppertal.de>
2024-05-21 13:41:41 +0200

    bench steps mpi ocl added

--- benchmarks/bench_steps.cpp
+++ benchmarks/bench_steps.cpp
@@ -1,3 +1,4 @@
+#include "bench_util.hpp"
 #include "cli_args.hpp"
 #include "nanobench.h"
 
@@ -6,6 +7,7 @@
 
 #include <chrono>
 #include <iostream>
+#include <mpi.h>
 using namespace std::chrono_literals;
 
 #include "../src/mgcl/cuboid.hpp"
@@ -607,5 +609,149 @@ TEST_CASE("benchStepsProfile")
         std::cout << "m,n,o: " << m << "," << n << "," << o << std::endl;
         pd->printBestTimingsPerKernel();
         std::cout << "========" << std::endl;
+    }
+}
+
+/**
+ * @brief Measures each step of vcycle using the profiler, i.e. prints kernel timings only.
+ *
+ */
+TEST_CASE("benchStepsProfileMpi")
+{
+    using std::min;
+
+    if (CLI_ARGS::grids.size() == 0 && (CLI_ARGS::gridsMin.size() == 0 || CLI_ARGS::gridsMax.size() == 0))
+        throw "Need to specify at least one local grid size, e.g. using --grids 4,8,16 or --gridsMin 4,4,4 AND --gridsMax 32,32,32";
+
+    // build grids to be tested from CLI args
+    std::vector<std::vector<int>> gridsTBT;
+    for (auto N : CLI_ARGS::grids)
+        gridsTBT.push_back({N, N, N});
+    if (CLI_ARGS::gridsMin.size() > 0 && CLI_ARGS::gridsMax.size() > 0)
+        for (int m = CLI_ARGS::gridsMin[0]; m <= CLI_ARGS::gridsMax[0]; m *= 2)
+            for (int n = CLI_ARGS::gridsMin[1]; n <= CLI_ARGS::gridsMax[1]; n *= 2)
+                for (int o = CLI_ARGS::gridsMin[2]; o <= CLI_ARGS::gridsMax[2]; o *= 2)
+                    gridsTBT.push_back({m, n, o});
+
+    // Check if mpi is initialized
+    int isInitialized = 0;
+    MPI_Initialized(&isInitialized);
+    REQUIRE(isInitialized);
+
+    MPI_Comm mpi_comm = MPI_COMM_WORLD;
+
+    // Check number of processes
+    int mpi_size = -1;
+    MPI_Comm_size(mpi_comm, &mpi_size);
+    // REQUIRE(mpi_size == 1);
+
+    int periodic = 1;
+
+    /* MPI variables */
+    int mpi_rank;
+    int mpi_dims[3] = {0, 0, 0};
+    int mpi_periods[3] = {periodic, periodic, periodic};
+    int mpi_coords[3];
+
+    /* Initialize cartesian process grid */
+    MPI_Comm_size(mpi_comm, &mpi_size);
+    MPI_Dims_create(mpi_size, 3, mpi_dims);
+    MPI_Cart_create(mpi_comm, 3, mpi_dims, mpi_periods, 1, &mpi_comm);
+    MPI_Comm_rank(mpi_comm, &mpi_rank);
+    MPI_Cart_coords(mpi_comm, mpi_rank, 3, mpi_coords);
+
+    if (mpi_rank == 0)
+    {
+        std::cout << "Testing the following grid sizes" << std::endl;
+        for (auto gr : gridsTBT)
+        {
+            int m = gr[0];
+            int n = gr[1];
+            int o = gr[2];
+            std::cout << "  " << m << "," << n << "," << o << std::endl;
+        }
+    }
+
+    std::vector<bench_util::ResultGhostUpdateMpi> results;
+
+    bool printedGpu = false;
+    for (auto gr : gridsTBT)
+    {
+        int m = gr[0];
+        int n = gr[1];
+        int o = gr[2];
+        int mglob = m * mpi_dims[0];
+        int nglob = n * mpi_dims[1];
+        int oglob = o * mpi_dims[2];
+        int ghosts = 1;
+        int mgh = m + 2 * ghosts;
+        int ngh = n + 2 * ghosts;
+        int ogh = o + 2 * ghosts;
+
+        mgcl::BC bc = mgcl::BC::PERIODIC;
+        bool periodic = bc == mgcl::BC::PERIODIC;
+        int maxiter = 1;
+        int benchiters = CLI_ARGS::bench_iterations;
+
+        auto f = std::make_shared<mgcl::Cuboid>(m, n, o);
+        auto v = std::make_shared<mgcl::Cuboid>(m, n, o);
+        f->fillRandom(0, 10);
+
+        mgcl::Problem p(m, n, o, f, v, mglob, nglob, oglob);
+        p.setUseOpencl(true);
+        if (mgcl_test::TestUtility::deviceAvailable("Quadro", CL_DEVICE_TYPE_GPU))
+            p.setDeviceName("Quadro");
+        p.setSilent(true);
+        p.setProfilingEnabled(true);
+        p.setStencilType(mgcl::MGCL_VARYING);
+        p.getStencilValues()->fillRandom();
+        p.setMpiComm(mpi_comm);
+        p.init();
+
+        if (!printedGpu)
+        {
+            for (int i = 0; i < mpi_size; i++)
+            {
+                MPI_Barrier(mpi_comm);
+                if (i == mpi_rank)
+                {
+                    std::cout << "on rank " << mpi_rank << ", GPU info: ";
+                    p.getOpenCLHelper().outputDeviceInfo(p.getOpenCLHelper().getDeviceId());
+                }
+            }
+            printedGpu = true;
+        }
+
+        // Clear measurements from init
+        auto pd = p.getProfilingData();
+        pd->getMeasurements().clear();
+
+        auto& v0 = p.getLevelAt(0).getV();
+        auto& v0d = p.getLevelAt(0).getDVIn();
+        auto& fine = p.getLevelAt(0);
+        auto& coarse = p.getLevelAt(1);
+
+        for (int i = 0; i < benchiters; i++)
+        {
+            p.solve(true);
+            MPI_Barrier(mpi_comm);
+            // mgcl::MultigridEngine::jacobi(p, fine, maxiter, false);
+            // mgcl::MultigridEngine::residual(p, fine, true);
+            // mgcl::MultigridEngine::updateGhosts(p, v0d, nullptr, false);
+            // mgcl::MultigridEngine::restrict(fine, coarse, fine.getDR(), coarse.getDF());
+            // mgcl::MultigridEngine::prolongate(fine, coarse, fine.getDR(), coarse.getDVIn());
+        }
+        clFinish(p.getCommands());
+        MPI_Barrier(mpi_comm);
+
+        // Print profiling measurements
+        if (mpi_rank == 0)
+        {
+            std::cout << "m,n,o: " << m << "," << n << "," << o << std::endl;
+            pd->printBestTimingsPerKernel();
+            std::cout << "========" << std::endl;
+        }
+
+        MPI_Barrier(mpi_comm);
     }
 }
