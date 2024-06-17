0f52bea7eee6c938656a1a17bf56bef31220d14d
Simon Hoffmann <shoffmann@uni-wuppertal.de>
2024-06-17 15:12:33 +0200

    added bench stencil_arithmetic_ocl_only with cli args

--- benchmarks/bench_stencil_arithmetic.cpp
+++ benchmarks/bench_stencil_arithmetic.cpp
@@ -1,3 +1,4 @@
+#include "cli_args.hpp"
 #include "nanobench.h"
 
 #include "catch2/catch_test_macros.hpp"
@@ -9,8 +10,7 @@ using namespace std::chrono_literals;
 #include "../src/mgcl/stencil.hpp"
 #include "../test/test_utility.hpp"
 
-// TODO implement + add to cmake
-TEST_CASE("stencil arithmetic", "[console][fixedVsVarying]")
+TEST_CASE("stencil_arithmetic")
 {
     int N = GENERATE(8, 16, 32, 64);
     // int N = 16;
@@ -138,6 +138,108 @@ TEST_CASE("stencil arithmetic", "[console][fixedVsVarying]")
             std::string name = std::string("gpu var*fix, N = ")
                                    .append(std::to_string(N));
 
+            b.run(std::string(name).c_str(), [&]
+                  { 
+                    ankerl::nanobench::doNotOptimizeAway(fd.multiply(vd, 0, tu->getProgram(), tu->getCommands(), tu->getContext(), nullptr, true, true, nullptr, nullptr)); 
+                    tu->finish(); });
+        }
+    }
+}
+
+TEST_CASE("stencil_arithmetic_ocl_only")
+{
+    if (CLI_ARGS::grids.size() == 0 && (CLI_ARGS::gridsMin.size() == 0 || CLI_ARGS::gridsMax.size() == 0))
+        throw "Need to specify at least one local grid size, e.g. using --grids 4,8,16 or --gridsMin 4,4,4 AND --gridsMax 32,32,32";
+
+    // build grids to be tested from CLI args
+    std::vector<std::vector<int>> gridsTBT;
+    for (auto N : CLI_ARGS::grids)
+        gridsTBT.push_back({N, N, N});
+    if (CLI_ARGS::gridsMin.size() > 0 && CLI_ARGS::gridsMax.size() > 0)
+        for (int m = CLI_ARGS::gridsMin[0]; m <= CLI_ARGS::gridsMax[0]; m *= 2)
+            for (int n = CLI_ARGS::gridsMin[1]; n <= CLI_ARGS::gridsMax[1]; n *= 2)
+                for (int o = CLI_ARGS::gridsMin[2]; o <= CLI_ARGS::gridsMax[2]; o *= 2)
+                    gridsTBT.push_back({m, n, o});
+
+    for (auto gr : gridsTBT)
+    {
+        int m = gr[0];
+        int n = gr[1];
+        int o = gr[2];
+
+        ankerl::nanobench::Bench b;
+        b.timeUnit(1ms, "ms")
+            .epochs(CLI_ARGS::bench_epochs)
+            .epochIterations(CLI_ARGS::bench_iterations)
+            .minEpochTime(100ms)
+            .relative(false);
+
+        auto tu = std::make_shared<mgcl_test::TestUtility>();
+        bool gpuAvailable = tu->deviceAvailable("Quadro", CL_DEVICE_TYPE_GPU);
+
+        if (!gpuAvailable)
+            throw "No GPU found!";
+
+        std::string name_grid = std::to_string(m)
+                                    .append("_")
+                                    .append(std::to_string(n))
+                                    .append("_")
+                                    .append(std::to_string(o));
+
+        int gh = 1;
+        int width = 3;
+
+        {
+            mgcl::VaryingStencil v(m, n, o, width, gh, gh, gh);
+            mgcl::VaryingStencil f(m, n, o, width, gh, gh, gh);
+            v.fillRandom(0, 10);
+            f.fillRandom(0, 10);
+
+            mgcl::VaryingStencilGpu vd(m, n, o, width, gh, tu->getContext(), tu->getCommands(), tu->getProgram());
+            mgcl::VaryingStencilGpu fd(m, n, o, width, gh, tu->getContext(), tu->getCommands(), tu->getProgram());
+            vd.fill(v, tu->getCommands(), true);
+            fd.fill(f, tu->getCommands(), true);
+            tu->finish();
+
+            std::string name = std::string("gpu var*var_").append(name_grid);
+            b.run(std::string(name).c_str(), [&]
+                  { 
+                    ankerl::nanobench::doNotOptimizeAway(vd.multiply(fd, 0, tu->getProgram(), tu->getCommands(), tu->getContext(), nullptr, true, true, nullptr, nullptr)); 
+                    tu->finish(); });
+        }
+
+        {
+            mgcl::FixedStencil v(width);
+            mgcl::VaryingStencil f(m, n, o, width, gh, gh, gh);
+            v.fillRandom(0, 10);
+            f.fillRandom(0, 10);
+
+            mgcl::FixedStencilGpu vd(width, tu->getContext(), tu->getCommands(), tu->getProgram());
+            mgcl::VaryingStencilGpu fd(m, n, o, width, gh, tu->getContext(), tu->getCommands(), tu->getProgram());
+            vd.fill(v, tu->getCommands(), true);
+            fd.fill(f, tu->getCommands(), true);
+            tu->finish();
+
+            std::string name = std::string("gpu fix*var_").append(name_grid);
+            b.run(std::string(name).c_str(), [&]
+                  { 
+                    ankerl::nanobench::doNotOptimizeAway(vd.multiply(fd, 0, tu->getProgram(), tu->getCommands(), tu->getContext(), nullptr, true, true, nullptr, nullptr)); 
+                    tu->finish(); });
+        }
+
+        {
+            mgcl::FixedStencil v(width);
+            mgcl::VaryingStencil f(m, n, o, width, gh, gh, gh);
+            v.fillRandom(0, 10);
+            f.fillRandom(0, 10);
+
+            mgcl::FixedStencilGpu vd(width, tu->getContext(), tu->getCommands(), tu->getProgram());
+            mgcl::VaryingStencilGpu fd(m, n, o, width, gh, tu->getContext(), tu->getCommands(), tu->getProgram());
+            vd.fill(v, tu->getCommands(), true);
+            fd.fill(f, tu->getCommands(), true);
+            tu->finish();
+
+            std::string name = std::string("gpu var*fix_").append(name_grid);
             b.run(std::string(name).c_str(), [&]
                   { 
                     ankerl::nanobench::doNotOptimizeAway(fd.multiply(vd, 0, tu->getProgram(), tu->getCommands(), tu->getContext(), nullptr, true, true, nullptr, nullptr)); 
