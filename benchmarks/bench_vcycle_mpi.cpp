048bb1672058768c43494b46895077b9eb80275a
Simon Hoffmann <shoffmann@uni-wuppertal.de>
2025-01-15 08:41:15 +0100

    bench benchmark_vcycle_MPI_OCL_fixed_vs_varying added

--- benchmarks/bench_vcycle_mpi.cpp
+++ benchmarks/bench_vcycle_mpi.cpp
@@ -748,3 +748,159 @@ TEST_CASE("benchmark_vcycle_MPI_OCL_galerkin_jacobi_iters")
         }
     }
 }
+
+// Benchmarks FixedStencil vs. VaryingStencil using OpenCL.
+// Only rank 0 will print the timings. Can also be run with only 1 MPI process.
+// Run with e.g.: mpiexec -n 4 benchmarks benchmark_vcycle_MPI_OCL_fixed_vs_varying
+TEST_CASE("benchmark_vcycle_MPI_OCL_fixed_vs_varying")
+{
+    using std::min;
+
+    if (CLI_ARGS::grids.size() == 0)
+        throw "Need to specify at least one local grid size, e.g. using --grids 4,8,16";
+
+    // Check if mpi is initialized
+    int isInitialized = 0;
+    MPI_Initialized(&isInitialized);
+    REQUIRE(isInitialized);
+
+    MPI_Comm mpi_comm = MPI_COMM_WORLD;
+
+    // Check number of processes
+    int mpi_size = -1;
+    MPI_Comm_size(mpi_comm, &mpi_size);
+    // REQUIRE(mpi_size == 1);
+
+    int periodic = 1;
+
+    /* MPI variables */
+    int mpi_rank;
+    int mpi_dims[3] = {0, 0, 0};
+    int mpi_periods[3] = {periodic, periodic, periodic};
+    int mpi_coords[3];
+
+    /* Initialize cartesian process grid */
+    MPI_Comm_size(mpi_comm, &mpi_size);
+    MPI_Dims_create(mpi_size, 3, mpi_dims);
+    MPI_Cart_create(mpi_comm, 3, mpi_dims, mpi_periods, 1, &mpi_comm);
+    MPI_Comm_rank(mpi_comm, &mpi_rank);
+    MPI_Cart_coords(mpi_comm, mpi_rank, 3, mpi_coords);
+
+    double omega = 0.8;
+    int nu1 = CLI_ARGS::nu1;
+    int nu2 = CLI_ARGS::nu2;
+    mgcl::MGCL_RESIDUAL_NORM resnorm = mgcl::MGCL_L2;
+
+    if (mpi_rank == 0)
+        std::cout << "Problem parameters:" << std::endl
+                  << "  Omega: " << omega << std::endl
+                  << "  VCycle iterations: " << CLI_ARGS::vCycleIterations << std::endl
+                  << "nu1: " << nu1 << ", nu2: " << nu2 << std::endl
+                  << "  #procs: " << mpi_size << std::endl;
+
+    std::vector<int> itersPerKernel{1, 2, 3};
+
+    for (auto N : CLI_ARGS::grids)
+    {
+        int mglob = N * mpi_dims[0];
+        int nglob = N * mpi_dims[1];
+        int oglob = N * mpi_dims[2];
+        double h = 1.0 / (double)mglob;
+
+        ankerl::nanobench::Bench bench;
+        bench.timeUnit(1ns, "ns")
+            .epochs(11)
+            .epochIterations(1)
+            // .minEpochTime(100ms)
+            .relative(true);
+
+        // disable output for non-root processes
+        if (mpi_rank > 0)
+            bench.output(nullptr);
+
+        int ghin = 0;
+
+        {
+            mgcl::MGCL_STENCIL stencilType = mgcl::MGCL_VARYING;
+
+            auto v = std::make_shared<mgcl::Cuboid>(N, N, N, ghin, ghin, ghin);
+            auto f = std::make_shared<mgcl::Cuboid>(N, N, N, ghin, ghin, ghin);
+            v->fillRandom();
+            f->fillRandom();
+
+            mgcl::Problem pocl(N, N, N, f, v, mglob, nglob, oglob);
+            pocl.setSilent(true);
+            // pocl.setMpiMinGridPoints(mgp);
+            pocl.setOmega(omega);
+            pocl.setNu1(nu1);
+            pocl.setNu2(nu2);
+            pocl.setMaxiterVcycles(CLI_ARGS::vCycleIterations);
+            pocl.setGhostsIn(ghin);
+            // pocl.setJacobiIterationsPerKernel(ipk);
+            // pocl.setGhosts(ipk);
+            pocl.setStencilType(stencilType);
+            pocl.setResidualNorm(resnorm);
+            pocl.setMpiComm(mpi_comm);
+            pocl.setUseOpencl(true);
+            pocl.setDeviceType(CL_DEVICE_TYPE_GPU);
+            pocl.setReadResults(true);
+
+            auto& sv = pocl.getStencilValues();
+            sv->fill1dIndex(true);
+
+            std::string name = std::string("ocl_varying_N_")
+                                   .append(std::to_string(N))
+                                   .append("_nu1_")
+                                   .append(std::to_string(nu1))
+                                   .append("_nu2_")
+                                   .append(std::to_string(nu2));
+
+            bench.run(std::string(name).c_str(), [&] { //
+                pocl.solve();
+                // tu.finish(); //
+                MPI_Barrier(mpi_comm);
+            });
+        }
+        {
+            mgcl::MGCL_STENCIL stencilType = mgcl::MGCL_FIXED;
+
+            auto v = std::make_shared<mgcl::Cuboid>(N, N, N, ghin, ghin, ghin);
+            auto f = std::make_shared<mgcl::Cuboid>(N, N, N, ghin, ghin, ghin);
+            v->fillRandom();
+            f->fillRandom();
+
+            mgcl::Problem pocl(N, N, N, f, v, mglob, nglob, oglob);
+            pocl.setSilent(true);
+            // pocl.setMpiMinGridPoints(mgp);
+            pocl.setOmega(omega);
+            pocl.setNu1(nu1);
+            pocl.setNu2(nu2);
+            pocl.setMaxiterVcycles(CLI_ARGS::vCycleIterations);
+            pocl.setGhostsIn(ghin);
+            // pocl.setJacobiIterationsPerKernel(ipk);
+            // pocl.setGhosts(ipk);
+            pocl.setStencilType(stencilType);
+            pocl.setResidualNorm(resnorm);
+            pocl.setMpiComm(mpi_comm);
+            pocl.setUseOpencl(true);
+            pocl.setDeviceType(CL_DEVICE_TYPE_GPU);
+            pocl.setReadResults(true);
+
+            auto& sv = pocl.getFixedStencil();
+            sv->fill1dIndex(true);
+
+            std::string name = std::string("ocl_fixed_N_")
+                                   .append(std::to_string(N))
+                                   .append("_nu1_")
+                                   .append(std::to_string(nu1))
+                                   .append("_nu2_")
+                                   .append(std::to_string(nu2));
+
+            bench.run(std::string(name).c_str(), [&] { //
+                pocl.solve();
+                // tu.finish(); //
+                MPI_Barrier(mpi_comm);
+            });
+        }
+    }
+}
