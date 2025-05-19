041acce9dc0cd07db6033bec2634d1218eee1de3
Simon Hoffmann <shoffmann@uni-wuppertal.de>
2025-05-19 13:08:55 +0200

    added mgcl_test::fill7pLaplace for Blockstencil

--- test/test_utility.cpp
+++ test/test_utility.cpp
@@ -317,6 +317,26 @@ void mgcl_test::fill7pLaplace(mgcl::VaryingStencil& v, double h, bool negativeCe
             }
 }
 
+void mgcl_test::fill7pLaplace(mgcl::Blockstencil& v, double h, bool negativeCenter)
+{
+    double f = negativeCenter ? -1.0 : 1.0;
+    double h2inv = f * (1.0 / (h * h));
+    for (size_t b = 0; b < v.getBlocksize(); b++)
+        for (int i = 0; i < v.getMgh(); i++)
+            for (int j = 0; j < v.getNgh(); j++)
+                for (int k = 0; k < v.getOgh(); k++)
+                {
+                    // 7-point Laplace
+                    v[b][b][0][1][1][i][j][k] = h2inv * -1.0;
+                    v[b][b][1][0][1][i][j][k] = h2inv * -1.0;
+                    v[b][b][1][1][0][i][j][k] = h2inv * -1.0;
+                    v[b][b][1][1][1][i][j][k] = h2inv * 6.0;
+                    v[b][b][1][1][2][i][j][k] = h2inv * -1.0;
+                    v[b][b][1][2][1][i][j][k] = h2inv * -1.0;
+                    v[b][b][2][1][1][i][j][k] = h2inv * -1.0;
+                }
+}
+
 void mgcl_test::fill7pLaplace(mgcl::FixedStencil& v, double h, bool negativeCenter)
 {
     double f = negativeCenter ? -1.0 : 1.0;
--- test/test_utility.hpp
+++ test/test_utility.hpp
@@ -54,6 +54,7 @@ namespace mgcl_test
 
     void create4hOrderPeriodicProblem(mgcl::Cuboid& v, mgcl::Cuboid& f, mgcl::Cuboid& solution);
     void fill7pLaplace(mgcl::VaryingStencil& v, double h, bool negativeCenter);
+    void fill7pLaplace(mgcl::Blockstencil& v, double h, bool negativeCenter);
     void fill19pLaplace(mgcl::VaryingStencil& v, double h, bool negativeCenter);
     void fill27pLaplace(mgcl::VaryingStencil& v, double h, bool negativeCenter);
     void fill7pLaplace(mgcl::FixedStencil& v, double h, bool negativeCenter);
